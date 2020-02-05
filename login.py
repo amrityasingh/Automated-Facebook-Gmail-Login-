314f36282fde4ba517cd0ba0e26f5e8d330593e9
Amritya Singh <46043903+awkwardamritya@users.noreply.github.com>
2020-02-06 00:46:49 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,6 @@
 from selenium import webdriver 
 from time import sleep
 import subprocess as sp
-#from cryptography.fernet import Fernet
-#import json
-
-#file = open('key.key','rb')
-#key = file.read()
-#file.close()
-
-#f = Fernet(key)
 
 x = sp.getoutput("whoami")
 user = "bananaboi"
@@ -18,55 +10,10 @@ if(x==user):
         print("Welcome {}".format(x))
         stat = 1
 
-#if(stat == 0):
-    #print("Hello {}!".format(x))
-    #print("Welcome to EasyLog. I can help you set up one command login")
-    #print("Set login info\n")
-    #sp.getoutput("echo { | cat > userinfo")
-    #print("1. Facebook \n")
-    #ch = int(input())
-    #print("Enter username")
-    #fb_u = str(input())
-    #fb_u_bytes = fb_u.encode()
-    #print("Enter password")
-    #fb_p = str(input())
-    #fb_p_bytes = fb_p.encode()
-    #encofb_u = f.encrypt(fb_u_bytes)
-    #encofb_p = f.encrypt(fb_p_bytes)
-    #sp.getoutput("printf '\"fbu\": \"{}\",' | cat >> userinfo".format(encofb_u))
-    #sp.getoutput("printf '\"fbp\": \"{}\",' | cat >> userinfo".format(encofb_p))
-#    print("2. Gmail \n")
-#    print("Enter username")
-#    #gm_u = str(input())
-    #gm_u_bytes = gm_u.encode()
-#    print("Enter password")
-    #gm_p = str(input())
-    #gm_p_bytes = gm_p.encode()
-    #encogm_u = f.encrypt(gm_u_bytes)
-    #encogm_p = f.encrypt(gm_p_bytes)
-    
-    #sp.getoutput("printf '\"gmu\": \"{}\",' | cat >> userinfo".format(encogm_u))
-
-    #sp.getoutput("printf '\"gmp\": \"{}\"'| cat >> userinfo".format(encogm_p))
-    #x = user
-    #sp.getoutput("printf '}' | cat >> userinfo")
-    #sp.getoutput("chmod 700 userinfo.json") 
-
-
-#with open('userinfo') as json_file:
-    #data = json.load(json_file)
-    
-
 print("Enter platform name to login into it ")
 c=str(input())
 
-if('facebook' == c):
-        #v1 = data["fbu"]
-        #v11 = v1.encode()
-        #usr= f.decrypt(v11)
-        #v2 = data["fbp"]
-        #v22 = v2.encode()
-        #pwd= f.decrypt(v22)
+if(c=='facebook'):
         usr='amritya.lordstark@gmail.com'
         pwd='xoxoxo69'
 
@@ -88,15 +35,7 @@ if('facebook' == c):
 
 
 elif(c=='gmail'):
-        #v3 = data["gmu"]
-        #v33 = v3.encode()
-        #v4 = data["gmp"]
-        #v44 = v4.encode()
-        #v333 = f.extract_timestamp(v33)
-        #v333 = f.decrypt(v33)
-        #v444 = f.decrypt(v44)
-
-        #print(v333,v44)
+   
         usr='amritya.stark'
         pwd='Yuphoria6999'
         driver= webdriver.Chrome()
@@ -121,5 +60,5 @@ else:
 
 print ("Done") 
 input('Press anything to quit') 
-#driver.quit() 
+driver.quit() 
 print("Finished") 
